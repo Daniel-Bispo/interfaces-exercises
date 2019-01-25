13d35d17f89a077063167f03567f1d76ea277c46
Daniel Bispo <danielvbispo@outlook.com>
2019-01-24 22:31:40 -0200

    Ata entity class

--- src/model/entities/Ata.java
+++ src/model/entities/Ata.java
@@ -1,5 +1,78 @@
 package model.entities;
 
-public class Ata {
+import java.util.Date;
 
+public class Ata {
+	
+	private int id;
+	private int ataNumber;
+	private String info;
+	private Date createDate;
+	private Date updateDate;
+	private String userLoggin;
+	
+	public Ata() {}
+
+	public Ata(int id, int ataNumber, String info, Date createDate, Date updateDate, String userLoggin) {
+		this.id = id;
+		this.ataNumber = ataNumber;
+		this.info = info;
+		this.createDate = createDate;
+		this.updateDate = updateDate;
+		this.userLoggin = userLoggin;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getAtaNumber() {
+		return ataNumber;
+	}
+
+	public void setAtaNumber(int ataNumber) {
+		this.ataNumber = ataNumber;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public String getUserLoggin() {
+		return userLoggin;
+	}
+
+	public void setUserLoggin(String userLoggin) {
+		this.userLoggin = userLoggin;
+	}
+
+	@Override
+	public String toString() {
+		return "Ata [id=" + id + ", ataNumber=" + ataNumber + ", info=" + info + ", createDate=" + createDate
+				+ ", updateDate=" + updateDate + ", userLoggin=" + userLoggin + "]";
+	}
 }
