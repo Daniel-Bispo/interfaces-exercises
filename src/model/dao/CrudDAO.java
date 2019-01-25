d667baa58b27188b4876c11e1588c8f6faa240bf
Daniel Bispo <danielvbispo@outlook.com>
2019-01-25 15:26:39 -0200

    CrudDAO interface

---
+++ src/model/dao/CrudDAO.java
@@ -0,0 +1,29 @@
+/**
+ * CrudDAO.java
+ *
+ *  @author Daniel Bispo <danielvbispo@outlook.com>
+ *  Created on 25 de jan de 2019
+ *  GNU License
+ *
+ */
+package model.dao;
+
+import java.util.List;
+
+/**
+ * CrudDAO declares all operations with the entities Using type T enables
+ * CrudDAO's flexibility
+ * Insert, update and delete any type of object
+ */
+public interface CrudDAO<T> {
+
+	void insert(T obj);
+
+	void upDate(T obj);
+
+	void deleteById(T obj);
+
+	T findById(int id); // Return any type of object
+
+	List<T> findAll(); // Return a list of any type of object
+}
---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,17 @@
+/**
+ * DaoFactory.java
+ *
+ *  @author Daniel Bispo <danielvbispo@outlook.com>
+ *  Created on 25 de jan de 2019
+ *  GNU License
+ *
+ */
+package model.dao;
+
+/**
+* It instantiates all implementation of entities DAO.
+* DaoFactory makes DAO more flexible for any type of database management system 
+*/
+public class DaoFactory {
+
+}
