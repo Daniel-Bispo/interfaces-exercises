c6aa95824891fb42b15d1b65cd0f8866cbcc8fcf
Daniel Bispo <danielvbispo@outlook.com>
2019-01-25 12:15:37 -0200

    DbException class implementation

---
+++ src/db/DdException.java
@@ -0,0 +1,21 @@
+/**
+ * DdException.java
+ *
+ *  @author Daniel Bispo <danielvbispo@outlook.com>
+ *  Created on 25 de jan de 2019
+ *  GNU License
+ *
+ */
+package db;
+
+/**
+*It throws all exceptions occurred during DataBase connections
+*/
+public class DdException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public DdException(String msg) {
+		super(msg);
+	}
+}
